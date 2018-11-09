49aadc240355073226fbd0e34c4afd966e591fac
tejashah94 <44180829+tejashah94@users.noreply.github.com>
2018-11-09 09:58:40 -0600

    Add files via upload

--- Phase_2/HDU.v
+++ Phase_2/HDU.v
@@ -1,4 +1,6 @@
-module HDU (IF_ID_Inst, ID_EX_MemRead, ID_EX_RegWrite, EX_MEM_RegWrite, EX_MEM_RdAddr, br_true, stall, IF_Flush, ID_Flush);
+//CORRECT BRANCH STALLS.
+
+module HDU (IF_ID_Inst, ID_EX_MemRead, ID_EX_RegWrite, EX_MEM_RegWrite, EX_MEM_RdAddr, br_true, ID_EX_flag_br_checker, EX_MEM_flag_br_checker, MEM_WB_flag_br_checker, stall, IF_Flush, ID_Flush);
 
 input [15:0] IF_ID_Inst;
 input ID_EX_MemRead;
@@ -6,6 +8,9 @@ input ID_EX_RegWrite;
 input EX_MEM_RegWrite;
 input [3:0] EX_MEM_RdAddr;
 input br_true;
+input ID_EX_flag_br_checker;
+input EX_MEM_flag_br_checker;
+input MEM_WB_flag_br_checker;
 output stall;
 output IF_Flush;
 output ID_Flush;
@@ -21,8 +26,8 @@ assign IF_ID_RegisterRt = (IF_ID_Inst[15:12] == 4'b1000 | IF_ID_Inst[15:12] == 4
 assign ID_EX_RegisterRt = ID_EX_MemRead ? IF_ID_Inst[11:8] : IF_ID_Inst[3:0];
 assign EX_MEM_RegisterRd = EX_MEM_RdAddr;
 //Data Hazard
-assign ID_Flush = (IF_ID_Inst[15] == 1'b0 | IF_ID_Inst[15:12] == 4'b1000 | IF_ID_Inst[15:12] == 4'b1001 | IF_ID_Inst[15:12] == 4'b1101) ? ((ID_EX_MemRead & ((ID_EX_RegisterRt == IF_ID_RegisterRs) | (ID_EX_RegisterRt == IF_ID_RegisterRt))) | ((IF_ID_Inst[15:12] ==  4'b1101)) | ((IF_ID_Inst[15:12] == 4'b1101))) ? 1'b1 : 1'b0 : 1'b0;
-assign stall = (IF_ID_Inst[15] == 1'b0 | IF_ID_Inst[15:12] == 4'b1000 | IF_ID_Inst[15:12] == 4'b1001 | IF_ID_Inst[15:12] == 4'b1101) ? ((ID_EX_MemRead & ((ID_EX_RegisterRt == IF_ID_RegisterRs) | (ID_EX_RegisterRt == IF_ID_RegisterRt))) | ((IF_ID_Inst[15:12] ==  4'b1101)) | ((IF_ID_Inst[15:12] == 4'b1101))) ? 1'b1 : 1'b0 : 1'b0;
+assign ID_Flush = (IF_ID_Inst[15] == 1'b0 | IF_ID_Inst[15:12] == 4'b1000 | IF_ID_Inst[15:12] == 4'b1001 | IF_ID_Inst[15:13] == 4'b110) ? ((ID_EX_MemRead & ((ID_EX_RegisterRt == IF_ID_RegisterRs) | (ID_EX_RegisterRt == IF_ID_RegisterRt))) | ((IF_ID_Inst[15:13] ==  3'b110 && ID_EX_flag_br_checker == 1'b1 && IF_ID_Inst[15:13] != 3'b111) | (IF_ID_Inst[15:13] ==  3'b110 && EX_MEM_flag_br_checker == 1'b1 && IF_ID_Inst[15:13] != 3'b111) | (IF_ID_Inst[15:13] ==  3'b110 && MEM_WB_flag_br_checker == 1'b1 && IF_ID_Inst[15:13] != 3'b111))) ? 1'b1 : 1'b0 : 1'b0;
+assign stall = (IF_ID_Inst[15] == 1'b0 | IF_ID_Inst[15:12] == 4'b1000 | IF_ID_Inst[15:12] == 4'b1001 | IF_ID_Inst[15:12] == 4'b1101) ? ((ID_EX_MemRead & ((ID_EX_RegisterRt == IF_ID_RegisterRs) | (ID_EX_RegisterRt == IF_ID_RegisterRt))) | ((IF_ID_Inst[15:13] ==  3'b110 && ID_EX_flag_br_checker == 1'b1 && IF_ID_Inst[15:13] != 3'b111) | (IF_ID_Inst[15:13] ==  3'b110 && EX_MEM_flag_br_checker == 1'b1 && IF_ID_Inst[15:13] != 3'b111) | (IF_ID_Inst[15:13] ==  3'b110 && MEM_WB_flag_br_checker == 1'b1 && IF_ID_Inst[15:13] != 3'b111))) ? 1'b1 : 1'b0 : 1'b0;
 
 //assign pc_write = ((ID_EX_MemRead & ((ID_EX_RegisterRt == IF_ID_RegisterRs) | (ID_EX_RegisterRt == IF_ID_RegisterRt))) | ((IF_ID_Inst[15:13] ==  3'b110) & ID_EX_RegWrite & ((ID_EX_RegisterRd == IF_ID_RegisterRs) | (ID_EX_RegisterRd == IF_ID_RegisterRt))) | ((IF_ID_Inst[15:13] ==  3'b110) & EX_MEM_RegWrite & ((EX_MEM_RegisterRd == IF_ID_RegisterRs) | (EX_MEM_RegisterRd == IF_ID_RegisterRt)))) ? 1'b1 : 1'b0; //PC stall for data hazards
 
