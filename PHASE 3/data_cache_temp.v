eedd72363f5e635f897920c189c4163d68c5f6ba
Shivangi Kamat <shivangi.kamat@gmail.com>
2018-11-29 00:18:08 -0800

    Update data_cache_temp.v

--- PHASE 3/data_cache_temp.v
+++ PHASE 3/data_cache_temp.v
@@ -1,6 +1,7 @@
+
 //Data Array instantiations
 
-
+/*
 module Data_cache(clk, rst, DataIn, Shift_out, miss_data_cache, data_addr);
 input clk;
 input rst;
@@ -47,24 +48,27 @@ DataArray DA0(.clk(clk), .rst(rst), .DataIn(DataIn_DA), .Write(Write_en_DA), .Bl
 
 endmodule
 
-
+*/
 
 //16 bit blocks
-module Data_cache(clk, rst, Data_Tag, Shift_out, miss_data_cache, data_addr);
+module Data_cache(clk, rst, Data_Tag, Shift_out, write_tag_array, miss_data_cache, data_addr);
 input clk;
 input rst;
 input [5:0] Data_Tag; //LRU, valid, tag
 input [127:0] Shift_out; //from Shifter_128bit
+input [15:0] data_addr;
 wire [127:0] BlockEnable;
 wire [15:0] DataOut;
+input write_tag_array;
 assign BlockEnable = Shift_out | (Shift_out << 1);
 //wire [1:0] hit;
-wire hit;
-wire [15:0] DataIn;
-wire Write_en;
+reg hit;
+reg [15:0] DataIn;
+reg Write_en;
+output reg miss_data_cache;
 
 MetaDataArray(.clk(clk), .rst(~rst), .DataIn(DataIn), .Write(Write_en), .BlockEnable(BlockEnable), .DataOut(DataOut));
-
+/*
 assign hit = (DataOut [14] & (DataOut[13:8] == Data_Tag)) ? 2'b10 : (DataOut[6] & (DataOut[5:0] == Data_Tag)) ? 2'b01 : 2'b00;
 
 assign DataIn = (hit == 2'b10) ? {1'b0, DataOut[14:8], 1'b1, DataOut[6:0]} :
@@ -75,8 +79,8 @@ assign DataIn = (hit == 2'b10) ? {1'b0, DataOut[14:8], 1'b1, DataOut[6:0]} :
                 {1'b1, DataOut[14:8], 1'b0, 1'b1, Data_Tag};
                 
 assign Write_en = ((hit == 2'b10) | (hit == 2'b01)) ? 1'b1 : write_tag_array;
-
-always @ (data_addr) begin
+*/
+always @ (*) begin
 
  case(DataOut[14] && (DataOut[13:8] == Data_Tag))
    1'b1:  begin hit = 1'b1;
@@ -85,21 +89,22 @@ always @ (data_addr) begin
           end
    1'b0:  begin
           case(DataOut[6] && (DataOut[5:0] == Data_Tag))
-            1'b1: begin hit = 1'b1;
-            DataIn = {1'b1, DataOut[14:8], 1'b0, DataOut[6:0]}
-            Write_en = 1'b1;
+            1'b1: begin 
+		hit = 1'b1;
+            	DataIn = {1'b1, DataOut[14:8], 1'b0, DataOut[6:0]};
+           	Write_en = 1'b1;
             end
             1'b0: begin
-            miss_data_cache = 1'b1;
-            Write_en = write_tag_array;
-            case(!DataOut[14]) 
-              1'b1: DataIn = {1'b0, 1'b1, Data_Tag, 1'b1, DataOut[6:0]};
-              1'b0: begin
-                     case(DataOut[15])
-                     1'b1: DataIn = {1'b0, 1'b1, Data_Tag, 1'b1, DataOut[6:0]};
-                     1'b0: DataIn = {1'b1, DataOut[14:8], 1'b0, 1'b1, Data_Tag};
-                     endcase
-                    end
+            	miss_data_cache = 1'b1;
+            	Write_en = write_tag_array;
+            		case(DataOut[14])  // check the valid bit of Block 1
+              			1'b0: DataIn = {1'b0, 1'b1, Data_Tag, 1'b1, DataOut[6:0]};
+             		 	1'b1: begin
+                		     case(DataOut[15])	// check the lru if valid is 1 for block 1
+                			     1'b1: DataIn = {1'b0, 1'b1, Data_Tag, 1'b1, DataOut[6:0]};		// if this is lru then evict
+                			     1'b0: DataIn = {1'b1, DataOut[14:8], 1'b0, 1'b1, Data_Tag};	// if this is not lru then irrespective of valid bit evict the other block
+                    			endcase
+                   			end
                 endcase
               end
             endcase
