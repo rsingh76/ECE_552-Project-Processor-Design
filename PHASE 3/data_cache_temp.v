1ac586706d4a4b6825d652216a72f02c3ec8c8d8
Shivangi Kamat <shivangi.kamat@gmail.com>
2018-11-29 15:30:40 -0800

    Update data_cache_temp.v

--- PHASE 3/data_cache_temp.v
+++ PHASE 3/data_cache_temp.v
@@ -51,7 +51,7 @@ endmodule
 */
 
 //16 bit blocks
-module Data_cache(clk, rst, Data_Tag, Shift_out, write_tag_array, miss_data_cache, data_addr);
+module Data_cache(clk, rst, Data_Tag, Shift_out, write_tag_array, Mem_write, DataIn_DA, write_data_array, miss_data_cache, data_addr, DataOut_DA);
 input clk;
 input rst;
 input [5:0] Data_Tag; //LRU, valid, tag
@@ -60,14 +60,25 @@ input [15:0] data_addr;
 wire [127:0] BlockEnable;
 wire [15:0] DataOut;
 input write_tag_array;
+input Mem_write;
 assign BlockEnable = Shift_out | (Shift_out << 1);
 //wire [1:0] hit;
 reg hit;
 reg [15:0] DataIn;
 reg Write_en;
+reg offset; //Tells which block is hit
 output reg miss_data_cache;
+//Data array stuff
+input [15:0] DataIn_DA;
+input write_data_array;
+wire Write_en_DA;
+wire [127:0] BlockEnable_DA;
+wire [7:0] WordEnable_DA;
+output [15:0] DataOut_DA;
 
-MetaDataArray(.clk(clk), .rst(~rst), .DataIn(DataIn), .Write(Write_en), .BlockEnable(BlockEnable), .DataOut(DataOut));
+MetaDataArray MDA1(.clk(clk), .rst(~rst), .DataIn(DataIn), .Write(Write_en), .BlockEnable(BlockEnable), .DataOut(DataOut));
+DataArray DA1(.clk(clk), .rst(~rst), .DataIn(DataIn_DA), .Write(Write_en_DA), .BlockEnable(BlockEnable_DA), .WordEnable(WordEnable_DA), .DataOut(DataOut_DA));
+
 /*
 assign hit = (DataOut [14] & (DataOut[13:8] == Data_Tag)) ? 2'b10 : (DataOut[6] & (DataOut[5:0] == Data_Tag)) ? 2'b01 : 2'b00;
 
@@ -80,12 +91,20 @@ assign DataIn = (hit == 2'b10) ? {1'b0, DataOut[14:8], 1'b1, DataOut[6:0]} :
                 
 assign Write_en = ((hit == 2'b10) | (hit == 2'b01)) ? 1'b1 : write_tag_array;
 */
-always @ (*) begin
+
+assign BlockEnable_DA = offset ? Shift_out : (Shift_out << 1);
+//assign mem_address = hit ? data_addr : miss_address;
+word_decoder WD1(.addr(data_addr[3:1]), .word_enable(WordEnable_DA));
+assign Write_en_DA = hit ? Mem_write : write_data_array;
+
+
+always @ (*) begin    //Think about default of case statements
 
  case(DataOut[14] && (DataOut[13:8] == Data_Tag))
    1'b1:  begin hit = 1'b1;
           DataIn = {1'b0, DataOut[14:8], 1'b1, DataOut[6:0]};
           Write_en = 1'b1;
+	  offset = 1'b1; //Hit in Block 1
           end
    1'b0:  begin
           case(DataOut[6] && (DataOut[5:0] == Data_Tag))
@@ -93,6 +112,7 @@ always @ (*) begin
 		hit = 1'b1;
             	DataIn = {1'b1, DataOut[14:8], 1'b0, DataOut[6:0]};
            	Write_en = 1'b1;
+		offset = 1'b0; //Hit in Block 0
             end
             1'b0: begin
             	miss_data_cache = 1'b1;
