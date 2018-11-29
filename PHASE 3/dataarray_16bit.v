b4d1ff4dfc9a5caeffcb3e33a1c14f51c370fdf3
Shivangi Kamat <shivangi.kamat@gmail.com>
2018-11-29 01:56:27 -0600

    Create dataarray_16bit.v

---
+++ PHASE 3/dataarray_16bit.v
@@ -0,0 +1,20 @@
+//Tag Array of 128  blocks
+//Each block will have 1 byte
+//BlockEnable is one-hot
+//WriteEnable is one on writes and zero on reads
+
+module MetaDataArray(input clk, input rst, input [15:0] DataIn, input Write, input [127:0] BlockEnable, output [15:0] DataOut);
+	MBlock Mblk0[63:0]( .clk(clk), .rst(rst), .Din(DataIn[7:0]), .WriteEnable(Write), .Enable(BlockEnable[63:0]), .Dout(DataOut[7:0]));
+  MBlock Mblk1[63:0]( .clk(clk), .rst(rst), .Din(DataIn[15:8]), .WriteEnable(Write), .Enable(BlockEnable[127:64]), .Dout(DataOut[15:8]));
+endmodule
+
+module MBlock( input clk,  input rst, input [7:0] Din, input WriteEnable, input Enable, output [7:0] Dout);
+	MCell mc[7:0]( .clk(clk), .rst(rst), .Din(Din[7:0]), .WriteEnable(WriteEnable), .Enable(Enable), .Dout(Dout[7:0]));
+endmodule
+
+module MCell( input clk,  input rst, input Din, input WriteEnable, input Enable, output Dout);
+	wire q;
+	assign Dout = (Enable & ~WriteEnable) ? q:'bz;
+	dff dffm(.q(q), .d(Din), .wen(Enable & WriteEnable), .clk(clk), .rst(rst));
+endmodule
+
