b6ac8c383cb6eadb22ee8bb68c684cf41568118b
tejashah94 <44180829+tejashah94@users.noreply.github.com>
2018-12-01 20:49:46 -0600

    Add files via upload

--- PHASE 3/MetaDataArray_t.v
+++ PHASE 3/MetaDataArray_t.v
@@ -7,15 +7,15 @@ module MetaDataArray_t(input clk, input rst, input [15:0] DataIn, input Write, i
 	MSet mset0[63:0]( .clk(clk), .rst(rst), .Din(DataIn[15:0]), .WriteEnable(Write), .Lru_en(Lru_en), .Enable(BlockEnable[63:0]), .Dout(DataOut[15:0]));
 endmodule
 
-module MSet(input clk, input rst, input [15:0] DataIn, input Write, input Lru_en, input Enable, output [15:0] DataOut);
-	MBlock Mblk0 ( .clk(clk), .rst(rst), .Din(DataIn[7:0]), .WriteEnable(Write), .Lru_en(Lru_en), .Enable(Enable), .Dout(DataOut[7:0]));
-	MBlock Mblk1 ( .clk(clk), .rst(rst), .Din(DataIn[15:8]), .WriteEnable(Write), .Lru_en(Lru_en), .Enable(Enable), .Dout(DataOut[15:8]));
+module MSet(input clk, input rst, input [15:0] Din, input WriteEnable, input Lru_en, input Enable, output [15:0] Dout);
+	MBlock Mblk0 ( .clk(clk), .rst(rst), .Din(Din[7:0]), .WriteEnable(WriteEnable), .Lru_en(Lru_en), .Enable(Enable), .Dout(Dout[7:0]));
+	MBlock Mblk1 ( .clk(clk), .rst(rst), .Din(Din[15:8]), .WriteEnable(WriteEnable), .Lru_en(Lru_en), .Enable(Enable), .Dout(Dout[15:8]));
 endmodule
 
 
-module MBlock( input clk,  input rst, input [7:0] Din, input WriteEnable, input Lru_en, input Enable, output [7:0] Dout);
-	MCell mc[6:0]( .clk(clk), .rst(rst), .Din(Din[6:0]), .WriteEnable(WriteEnable), .Enable(Enable), .Dout(Dout[7:0]));
-        MCell_lru lru( .clk(clk), .rst(rst), .Din(Din[7]), .Lru_en(Lru_en), .Enable(Enable), .Dout(Dout[7:0]));
+module MBlock( input clk, input rst, input [7:0] Din, input WriteEnable, input Lru_en, input Enable, output [7:0] Dout);
+	MCell mc[6:0]( .clk(clk), .rst(rst), .Din(Din[6:0]), .WriteEnable(WriteEnable), .Enable(Enable), .Dout(Dout[6:0]));
+        MCell_lru lru( .clk(clk), .rst(rst), .Din(Din[7]), .WriteEnable(WriteEnable), .Lru_en(Lru_en), .Enable(Enable), .Dout(Dout[7]));
 endmodule
 
 module MCell( input clk,  input rst, input Din, input WriteEnable, input Enable, output Dout);
