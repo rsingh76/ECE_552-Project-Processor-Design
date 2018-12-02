9029183a00c234b44822438b11edb636bec10ef6
rsingh76 <44885148+rsingh76@users.noreply.github.com>
2018-12-01 22:40:40 -0600

    Add files via upload

--- PHASE 3/MetaData_Data.v
+++ PHASE 3/MetaData_Data.v
@@ -4,15 +4,15 @@
 //WriteEnable is one on writes and zero on reads
 
 module MetaDataArray_Data(input clk, input rst, input [15:0] DataIn, input Write, input [127:0] BlockEnable, output [15:0] DataOut);
-	MBlock Mblk0[63:0]( .clk(clk), .rst(rst), .Din(DataIn[7:0]), .WriteEnable(Write), .Enable(BlockEnable[63:0]), .Dout(DataOut[7:0]));
-  MBlock Mblk1[63:0]( .clk(clk), .rst(rst), .Din(DataIn[15:8]), .WriteEnable(Write), .Enable(BlockEnable[127:64]), .Dout(DataOut[15:8]));
+  MBlock_Data Mblk0[63:0]( .clk(clk), .rst(rst), .Din(DataIn[7:0]), .WriteEnable(Write), .Enable(BlockEnable[63:0]), .Dout(DataOut[7:0]));
+  MBlock_Data Mblk1[63:0]( .clk(clk), .rst(rst), .Din(DataIn[15:8]), .WriteEnable(Write), .Enable(BlockEnable[127:64]), .Dout(DataOut[15:8]));
 endmodule
 
-module MBlock( input clk,  input rst, input [7:0] Din, input WriteEnable, input Enable, output [7:0] Dout);
-	MCell mc[7:0]( .clk(clk), .rst(rst), .Din(Din[7:0]), .WriteEnable(WriteEnable), .Enable(Enable), .Dout(Dout[7:0]));
+module MBlock_Data( input clk,  input rst, input [7:0] Din, input WriteEnable, input Enable, output [7:0] Dout);
+	MCell_Data mc[7:0]( .clk(clk), .rst(rst), .Din(Din[7:0]), .WriteEnable(WriteEnable), .Enable(Enable), .Dout(Dout[7:0]));
 endmodule
 
-module MCell( input clk,  input rst, input Din, input WriteEnable, input Enable, output Dout);
+module MCell_Data( input clk,  input rst, input Din, input WriteEnable, input Enable, output Dout);
 	wire q;
 	assign Dout = (Enable & ~WriteEnable) ? q:'bz;
 	dff dffm(.q(q), .d(Din), .wen(Enable & WriteEnable), .clk(clk), .rst(rst));
