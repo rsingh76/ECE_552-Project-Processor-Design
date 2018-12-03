7bfb382d4ba2a2ce41ac02d534f984ecae928495
Shivangi Kamat <shivangi.kamat@gmail.com>
2018-12-02 16:15:15 -0800

    Update data_cache_temp.v

--- PHASE 3/data_cache_temp.v
+++ PHASE 3/data_cache_temp.v
@@ -57,11 +57,15 @@ input rst;
 input [5:0] Data_Tag; //LRU, valid, tag
 input [127:0] Shift_out; //from Shifter_128bit
 input [15:0] data_addr;
-wire [127:0] BlockEnable;
+wire [63:0] BlockEnable_0; //Blockenables for Set0 and Set1 of MetaData Array
+wire [63:0] BlockEnable_1;
 wire [15:0] DataOut;
 input write_tag_array;
 input Mem_write;
-assign BlockEnable = Shift_out | (Shift_out << 1);
+
+assign BlockEnable_0 = Shift_out;
+assign BlockEnable_1 = Shift_out;
+//assign BlockEnable = Shift_out | (Shift_out << 1);
 //wire [1:0] hit;
 reg hit;
 reg [15:0] DataIn;
@@ -72,12 +76,12 @@ output reg miss_data_cache;
 input [15:0] DataIn_DA;
 input write_data_array;
 wire Write_en_DA;
-wire [127:0] BlockEnable_DA;
+wire [63:0] BlockEnable_DA;;
 wire [7:0] WordEnable_DA;
 output [15:0] DataOut_DA;
 
-MetaDataArray_Data MDA1(.clk(clk), .rst(~rst), .DataIn(DataIn), .Write(Write_en), .BlockEnable(BlockEnable), .DataOut(DataOut));
-DataArray DA1(.clk(clk), .rst(~rst), .DataIn(DataIn_DA), .Write(Write_en_DA), .BlockEnable(BlockEnable_DA), .WordEnable(WordEnable_DA), .DataOut(DataOut_DA));
+MetaDataArray_Data MDA1(.clk(clk), .rst(~rst), .DataIn(DataIn), .Write(Write_en), .BlockEnable_0(BlockEnable_0), .BlockEnable_1(BlockEnable_1), .DataOut(DataOut));
+DataArray DA1(.clk(clk), .rst(~rst), .DataIn(DataIn_DA), .Write(Write_en_DA), .BlockEnable(BlockEnable_DA), .offset(offset), .WordEnable(WordEnable_DA), .DataOut(DataOut_DA));
 
 /*
 assign hit = (DataOut [14] & (DataOut[13:8] == Data_Tag)) ? 2'b10 : (DataOut[6] & (DataOut[5:0] == Data_Tag)) ? 2'b01 : 2'b00;
@@ -92,7 +96,8 @@ assign DataIn = (hit == 2'b10) ? {1'b0, DataOut[14:8], 1'b1, DataOut[6:0]} :
 assign Write_en = ((hit == 2'b10) | (hit == 2'b01)) ? 1'b1 : write_tag_array;
 */
 
-assign BlockEnable_DA = offset ? Shift_out : (Shift_out << 1);
+//assign BlockEnable_DA = offset ? Shift_out : (Shift_out << 1);
+assign BlockEnable_DA = Shift_out;
 //assign mem_address = hit ? data_addr : miss_address;
 word_decoder WD1(.addr(data_addr[3:1]), .word_enable(WordEnable_DA));
 assign Write_en_DA = hit ? Mem_write : write_data_array;
@@ -100,6 +105,7 @@ assign Write_en_DA = hit ? Mem_write : write_data_array;
 
 always @ (*) begin    //Think about default of case statements
 miss_data_cache = 1'b0;
+offset = 1'b0;
  case(DataOut[14] && (DataOut[13:8] == Data_Tag))
    1'b1:  begin hit = 1'b1;
           DataIn = {1'b0, DataOut[14:8], 1'b1, DataOut[6:0]};
