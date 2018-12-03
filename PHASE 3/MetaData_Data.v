0a1dc6fe3f7df758bd714ff8e6f2298e56960fe0
Shivangi Kamat <shivangi.kamat@gmail.com>
2018-12-02 16:14:46 -0800

    Update MetaData_Data.v

--- PHASE 3/MetaData_Data.v
+++ PHASE 3/MetaData_Data.v
@@ -3,9 +3,9 @@
 //BlockEnable is one-hot
 //WriteEnable is one on writes and zero on reads
 
-module MetaDataArray_Data(input clk, input rst, input [15:0] DataIn, input Write, input [127:0] BlockEnable, output [15:0] DataOut);
-  MBlock_Data Mblk0[63:0]( .clk(clk), .rst(rst), .Din(DataIn[7:0]), .WriteEnable(Write), .Enable(BlockEnable[63:0]), .Dout(DataOut[7:0]));
-  MBlock_Data Mblk1[63:0]( .clk(clk), .rst(rst), .Din(DataIn[15:8]), .WriteEnable(Write), .Enable(BlockEnable[127:64]), .Dout(DataOut[15:8]));
+module MetaDataArray_Data(input clk, input rst, input [15:0] DataIn, input Write, input [63:0] BlockEnable_0, input [63:0] BlockEnable_1, output [15:0] DataOut);
+  MBlock_Data Mblk0[63:0]( .clk(clk), .rst(rst), .Din(DataIn[7:0]), .WriteEnable(Write), .Enable(BlockEnable_0[63:0]), .Dout(DataOut[7:0]));
+  MBlock_Data Mblk1[63:0]( .clk(clk), .rst(rst), .Din(DataIn[15:8]), .WriteEnable(Write), .Enable(BlockEnable_1[63:0]), .Dout(DataOut[15:8]));
 endmodule
 
 module MBlock_Data( input clk,  input rst, input [7:0] Din, input WriteEnable, input Enable, output [7:0] Dout);
@@ -17,4 +17,3 @@ module MCell_Data( input clk,  input rst, input Din, input WriteEnable, input En
 	assign Dout = (Enable & ~WriteEnable) ? q:'bz;
 	dff dffm(.q(q), .d(Din), .wen(Enable & WriteEnable), .clk(clk), .rst(rst));
 endmodule
-
